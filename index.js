cf01e0efbf51c6677f10fbade91afb7fd14022ef
Mohit Tanwani <mohitmurlidhar@gmail.com>
2019-05-25 22:26:11 +0530

    Resolved Access Issue

--- index.js
+++ index.js
@@ -14,18 +14,17 @@ app.get('/', (req, res) => {
 	fs.readFile("quotes.db", function(err, buf) {
 		var qarr=JSON.parse(buf);
 		var random=Math.floor(Math.random()*(qarr.length-1));
-		//Allow API
 		res.setHeader('Access-Control-Allow-Origin', '/');
 		// Request methods you wish to allow
-		res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
-		// Request headers you wish to allow
-		res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
-		// Set to true if you need the website to include cookies in the requests sent
-		// to the API (e.g. in case you use sessions)
-		res.setHeader('Access-Control-Allow-Credentials', true);
-		res.response(qarr[random]);
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+		res.send(qarr[random]);
 	});
 });
 
