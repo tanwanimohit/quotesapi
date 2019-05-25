07e6507d23c5398b250c027442f36ac41eda1cbe
Mohit Tanwani <mohitmurlidhar@gmail.com>
2019-05-25 22:50:48 +0530

    Added Game oF Throne API

--- index.js
+++ index.js
@@ -28,6 +28,28 @@ app.get('/', (req, res) => {
 	});
 });
 
+app.get('/got', (req, res) => {
+	
+	fs.readFile("quotes.db", function(err, buf) {
+		var qarr=JSON.parse(buf).filter(function (n){
+			return n.category==="got";
+		});;
+		var random=Math.floor(Math.random()*(qarr.length-1));
+		res.setHeader('Access-Control-Allow-Origin', '*');
+		// Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+		res.send(qarr[random]);
+	});
+});
+
+
 
 //Httpserver Port Number 3000.
 app.listen(process.env.PORT || 3000, function(){
